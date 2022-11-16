ab6c7aabb02537c5a86177a705f412e3638f79ef
Onur Kaya <kayaonur12@gmail.com>
2022-11-16 13:56:37 +0300

    another commit

--- src/Donguler/Power_of.java
+++ src/Donguler/Power_of.java
@@ -1,16 +1,18 @@
 package Donguler;
+
 import java.util.Scanner;
+
 public class Power_of {
     public static void main(String[] args) {
+        int number, power, i, total = 1;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int number = input.nextInt();
+        number = input.nextInt();
         System.out.print("Enter the power: ");
-        int power = input.nextInt();
-        int total=1;
+        power = input.nextInt();
 
-        for (int i =1;i<=power;i++)
-            total *=number;
-        System.out.println("Answer is: "+total);
+        for (i = 1; i <= power; i++)
+            total *= number;
+        System.out.println("Answer is: " + total);
     }
 }
